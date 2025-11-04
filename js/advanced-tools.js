65c02d745321336200bd0a37feb3b01076c82431
Paul Meyer <okovessimon@yahoo.de>
2025-11-04 13:59:37 +0100

    implement export names

--- js/advanced-tools.js
+++ js/advanced-tools.js
@@ -41,25 +41,49 @@ function importNames() {
     console.log("Namen importieren...");
 }
 
+function exportJSON(data, filename){
+    const json = JSON.stringify(data, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename + '.json';
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
 // Export seats as JSON
 function exportSeats() {
     try {
-        const seatData = getSeatData();
-        const json = JSON.stringify(seatData, null, 2);
-        const blob = new Blob([json], { type: 'application/json' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'seats.json';
-        a.click();
-        URL.revokeObjectURL(url);
+        const seatsData = getSeatData();
+        exportJSON(seatsData, "seats");
     } catch (err) {
         alert('Export fehlgeschlagen: ' + err.message);
     }
 }
 
-
+// Export names as JSON
 function exportNames() {
-  // TODO:
-  console.log("Namen exportieren...");
+    try{
+        const namesData = collectNamesData();
+        exportJSON(namesData, "names");
+    } catch (err) {
+        alert('Export fehlgeschlagen: ' + err.message);
+    }
+}
+
+function collectNamesData() {
+    const namesInputString = document.getElementById('namesInput').value || '';
+
+    const seats = document.querySelectorAll('#canvas .seat');
+    const seatNames = Array.from(seats).map(seat => {
+        const first = seat.querySelector('.seat-firstname')?.textContent.trim() || '';
+        const last = seat.querySelector('.seat-lastname')?.textContent.trim() || '';
+        return { firstname: first, lastname: last };
+    });
+
+    return namesData = {
+        'namesInput': namesInputString,
+        'seat-names': seatNames
+    };
 }
