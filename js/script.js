8e62725d9a8e9d6b36eab579390493e823788fef
Paul Meyer <okovessimon@yahoo.de>
2025-11-20 22:35:48 +0100

    respect lockedSeatTag when assigning names to seats

--- js/script.js
+++ js/script.js
@@ -463,10 +463,17 @@ async function loadData() {
 
 function shuffleArray(array) {
     const result = [...array];
-    for (let i = result.length - 1; i > 0; i--) {
+    const freeItems = result.filter(item => !item.lockedSeat);
+    for (let i = freeItems.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [result[i], result[j]] = [result[j], result[i]]; // swap elements
+        [freeItems[i], freeItems[j]] = [freeItems[j], freeItems[i]]; // swap elements
     }
+    
+    // Place free items back to next free seat positions
+    let freeIdx = 0;
+    result.forEach((item, i) => {
+        if (!item.lockedSeat) result[i] = freeItems[freeIdx++];
+    });
     return result;
 }
 
